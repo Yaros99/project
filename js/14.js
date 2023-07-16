7f3722d175521b3ec596bfb798f20ca511ec86b4
Yaros <example@gmail.comglobal>
2023-07-16 17:09:16 +0300

    first comit

---
+++ js/14.js
@@ -0,0 +1,9 @@
+'use strict';
+интерполяция, бектики; 
+const category = 'Toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+alert(`Привет, ${user}`);
+
---
+++ js/15.js
@@ -0,0 +1,28 @@
+/* eslint-disable indent */
+
+// конкатенация
+console.log('arr' + ' - object');
+console.log('4' + ' - object');
+
+let incr =10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+// && - и
+// || - или 
+// ! - отрицание ( меняет наоборот)
+
+const isChecked = false,
+     isClose = false;
+
+console.log(isChecked || !isClose);
+
